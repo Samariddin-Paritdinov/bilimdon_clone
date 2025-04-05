895fc505f984c3d03eddcdb1431acb4e3528163f
Samariddin-Paritdinov <muzaffarovichsamarr@gmail.com>
2025-04-05 17:46:23 +0500

    Fayl o‘zgartirildi yoki yangilandi

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import mapped_column, Mapped, relationship
-from sqlalchemy import String, DateTime, Date
+from sqlalchemy import String, DateTime, Date, ForeignKey, Integer, Text
 
-from datetime import datetime, date, timezone
+from typing import Optional
+from datetime import datetime, date, timezone, time
 
 from app.database import Base
 
@@ -21,3 +22,22 @@ class User(Base):
     is_staff: Mapped[bool] = mapped_column(default=False)
     is_superuser: Mapped[bool] = mapped_column(default=False)
 
+class Participation(Base):
+    __tablename__ = 'participations'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        type_=Integer,
+        nullable=True
+    )
+    owner: Mapped[Optional["User"]] = relationship("User")
+    topic_id: Mapped[int] = mapped_column()
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
+
+    game_id: Mapped[int] = mapped_column()
+    start_time: Mapped[time]
+    eng_time: Mapped[Optional[time]] = mapped_column(nullable=False)
+    gained_score: Mapped[int] = mapped_column(default=0)
